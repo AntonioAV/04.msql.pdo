fad5925dcd99a674ec31258cfb6fff6964f4ee8a
Antonio Arencibia <arencibiacasa@gmail.com>
2020-01-12 19:30:42 +0100

    Se crea el login

--- modelos/formularios.modelos.php
+++ modelos/formularios.modelos.php
@@ -32,13 +32,28 @@ class ModeloFormularios{
               Seleccionar Registros
              =======================================================*/
 
-    static public function mdlSeleccionarRegistros($tabla){
+    static public function mdlSeleccionarRegistros($tabla, $item, $valor){
 
-        $stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla");
+        if($item == null & $valor == null){
 
-        $stmt->execute();
+            $stmt = Conexion::conectar()->prepare("SELECT *,DATE_FORMAT(fecha, '%d/%m%Y') AS fecha FROM $tabla ORDER BY id DESC");
+    
+            $stmt->execute();
+    
+            return $stmt-> fetchAll();
 
-        return $stmt-> fetchAll();
+        } else {
+
+            $stmt = Conexion::conectar()->prepare("SELECT *,DATE_FORMAT(fecha, '%d/%m%Y') AS fecha FROM $tabla WHERE $item = :$item ORDER BY id DESC");
+
+            $stmt->bindParam(":" .$item, $valor, PDO::PARAM_STR);   
+    
+            $stmt->execute();
+    
+            return $stmt-> fetch();
+
+        }
+
 
         $stmt -> close();
 
